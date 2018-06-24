41a9ff8439b1c948cc7ab7d0e919ab54b7f65516
zhengjunhao <changchunjunhao@gmail.com>
2018-06-24 15:20:35 +0800

    simply add store_scene

--- card-game/scene/game/game_scene.py
+++ card-game/scene/game/game_scene.py
@@ -1,6 +1,10 @@
 
-from scene.game import charector_select_scene, map_scene
+from scene.game import charector_select_scene, map_scene, store_scene
 from record import *
+
+T_EVENT = map_scene.T_EVENT
+T_STORE = map_scene.T_STORE
+T_BATTLE = map_scene.T_BATTLE
 
 def runGame(surface, fpsclock, data) :
     if not hasattr(data.user, 'init') :
@@ -12,5 +16,17 @@ def runGame(surface, fpsclock, data) :
     while True:
         status = map_scene.show(surface, fpsclock, data)
         if status is map_scene.SELECT_FINISH :
-            break
+            s = data.map.selected_item
+            if s is None:
+                # game may be over but how could it be?
+                break
+
+            assert (s.type in [T_EVENT, T_STORE, T_BATTLE]), "unexpected map_item type meet: %s" % s.type
+            if s.type is T_EVENT:
+                nop = 1
+            elif s.type is T_STORE:
+                store_scene.show(surface, fpsclock, data)
+            elif s.type is T_BATTLE:
+                nop = 1
+
     print("game_scene is done\n")
---
+++ card-game/scene/game/store_scene.py
@@ -0,0 +1,6 @@
+
+import pygame
+from pygame.locals import *
+
+def show(surface, fpsclock, data):
+    print('showing store_scene')
